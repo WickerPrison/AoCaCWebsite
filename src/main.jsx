879b45d07035c3cfe9a476f633fee2c1ac3be14f
WickerPrison <cobylax17@gmail.com>
2024-05-09 15:56:59 -0600

    set up basics

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }
+    ],
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+)
