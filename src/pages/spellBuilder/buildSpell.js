b4a56799ca394cc36c5a5cb63d8b8aae59ac481f
WickerPrison <cobylax17@gmail.com>
2024-05-17 15:44:02 -0600

    handled double edge case

--- src/pages/spellBuilder/buildSpell.js
+++ src/pages/spellBuilder/buildSpell.js
@@ -39,13 +39,20 @@ function assembleDicePool(spellData){
         pool.upgradeDifficulty += 3;
     }
 
+    let double = false;
     // modifiers from spell effects
     for(let i = 0; i < spellData.currentEffects.length; i++){
-        pool.difficulty += Number(spellData.currentEffects[i].Difficulty) * spellData.currentEffects[i].nodes;
-        pool.upgradeDifficulty += Number(spellData.currentEffects[i].Upgrades) * spellData.currentEffects[i].nodes;
-        pool.penalty += Number(spellData.currentEffects[i].Penalty) * spellData.currentEffects[i].nodes;
-        if(spellData.currentEffects[i].SpecialModifier != ""){
-            pool = specialModifier(pool, spellData, i);
+        pool = applyNodes(pool, spellData, i);
+        if(spellData.currentEffects[i].SpecialModifier == "double"){
+            double = true;
+        }
+    }
+
+    if(double){
+        for(let i = 0; i < spellData.currentEffects.length; i++){
+            if(spellData.currentEffects[i].Duration != "Instantaneous"){
+                pool = applyNodes(pool, spellData, i);
+            }
         }
     }
     
@@ -78,10 +85,22 @@ function assembleDicePool(spellData){
     return pool;
 }
 
+function applyNodes(pool, spellData, i){
+    pool.difficulty += Number(spellData.currentEffects[i].Difficulty) * spellData.currentEffects[i].nodes;
+    pool.upgradeDifficulty += Number(spellData.currentEffects[i].Upgrades) * spellData.currentEffects[i].nodes;
+    pool.penalty += Number(spellData.currentEffects[i].Penalty) * spellData.currentEffects[i].nodes;
+    if(spellData.currentEffects[i].SpecialModifier != ""){
+        pool = specialModifier(pool, spellData, i);
+    }
+    return pool;
+}
+
 function specialModifier(pool, spellData, i){
     switch(spellData.currentEffects[i].SpecialModifier){
         case "boost":
             pool.boost += Number(spellData.currentEffects[i].nodes);
+            return pool;
+        default:
             return pool;
     }
 }
