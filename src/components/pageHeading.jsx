382b33ae52ad929b32248c7aa931822544031c23
WickerPrison <cobylax17@gmail.com>
2024-05-18 18:42:53 -0600

    solved deployment issues

--- src/components/pageHeading.jsx
+++ src/components/pageHeading.jsx
@@ -4,12 +4,12 @@ export default function PageHeading({title}) {
         <div>
             <h2 id="page-heading">{title}</h2>
             <section id="page-title">
-                <img className="square-die" src="/SVG/Boost.svg"/>
-                <img src={window.location.origin + "/SVG/Ability.svg"}/>
+                <img className="square-die" src="./SVG/Boost.svg"/>
+                <img src="./SVG/Ability.svg"/>
                 <img src="./SVG/Proficiency.svg"/>
-                <img src="/SVG/Challenge.svg"/>
-                <img src="/SVG/Difficulty.svg"/>
-                <img className="square-die" src="/SVG/Penalty.svg"/>
+                <img src="./SVG/Challenge.svg"/>
+                <img src="./SVG/Difficulty.svg"/>
+                <img className="square-die" src="./SVG/Penalty.svg"/>
             </section>
         </div>
     );
--- src/components/rollerResult.jsx
+++ src/components/rollerResult.jsx
@@ -5,7 +5,7 @@ export default function RollerResult({result, label}){
         <div className = "output-element">
             <h4>{label} </h4>
             <div className="dice-holder">
-                <img src="/SVG/d12.svg"/>
+                <img src="./SVG/d12.svg"/>
                 <h4 className="conquests">{result}</h4>
             </div>
         </div>
