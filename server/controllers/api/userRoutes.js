dae2ea760f2cc29350ccea473939d26082e66a9d
WickerPrison <cobylax17@gmail.com>
2024-07-05 07:44:04 -0600

    added user routes

---
+++ server/controllers/api/userRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+router.get('/', async (req, res) => {
+        const users = await User.find().catch((err) =>{
+            res.json(err)
+        });
+        res.json(users);
+})
+
+router.get('/:id', async (req, res) => {
+    const users = await User.findById(req.params.id).catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+router.post('/', async (req, res) => {
+    const user = await User.create(req.body).catch((err) =>{
+        res.json(err);
+    });
+    res.json(user);
+})
+
+router.put('/:id', async (req, res) => {
+    const users = await User.findByIdAndUpdate(req.params.id, 
+        {
+            username: req.body.username,
+            email: req.body.email
+        }, 
+        {new: true})
+        .catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+router.delete('/:id', async (req, res) =>{
+    const users = await User.findByIdAndDelete(req.params.id).catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+module.exports = router;
