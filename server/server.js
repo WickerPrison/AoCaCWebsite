f5263d5b8bbd96b607bb97514d080b332b1e7586
WickerPrison <cobylax17@gmail.com>
2024-07-10 08:42:33 -0600

    fix routes again

--- server/server.js
+++ server/server.js
@@ -8,17 +8,18 @@ const PORT = 3001;
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-
+  
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
