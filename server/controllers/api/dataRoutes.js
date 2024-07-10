4ac66de5fbabddb53df3ce5798ce64bec989b561
WickerPrison <cobylax17@gmail.com>
2024-07-10 10:17:54 -0600

    added log statements

--- server/controllers/api/dataRoutes.js
+++ server/controllers/api/dataRoutes.js
@@ -3,8 +3,10 @@ const SpellEffect = require('../../models/SpellEffect');
 
 router.get('/spellEffects', async (req, res) => {
     const spellEffects = await SpellEffect.find().catch((err) =>{
+        console.log("error", err);
         res.json(err)
     });
+    console.log("no error");
     res.json(spellEffects);
 });
 
