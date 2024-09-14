93b2aabb254294db941b950d5eb8826dac7c2787
WickerPrison <cobylax17@gmail.com>
2024-09-14 09:52:57 -0600

    railway problems

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 const bcrypt = require('bcrypt');
 const rollSchema = require('./Roll');
-const monsterDataSchema = require('./MonsterData');
+const monsterDataSchema = require('./MonsterData'); // with a capital M
 
 const userSchema = new Schema(
     {
